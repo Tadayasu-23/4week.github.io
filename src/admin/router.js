57b4b79e9977faad4515751773e722e5a130681a
Tadayasu <85864420+Tadayasu-23@users.noreply.github.com>
2021-07-11 22:39:27 +0300

    Update router.js

--- src/admin/router.js
+++ src/admin/router.js
@@ -21,4 +21,29 @@ const routes = [
   }
 ];
 
-export default new VueRouter({ routes });
+const baseURL = "https://webdev-api.loftschool.com";
+const loginPage = axios.create({ baseURL });
+
+const router = new VueRouter({ routes });
+
+router.beforeEach(async (to, from, next) => {
+   const isPublicRoute = to.path === '/login'
+ 
+   if (isPublicRoute === false) {
+     const token = localStorage.getItem('token');
+     loginPage.defaults.headers['Authorization'] = `Bearer ${token}`;
+ 
+     try {
+       await loginPage.get('/user');
+       next(); 
+     } catch (error) {
+       router.replace('/login');
+       localStorage.removeItem('token');
+     }
+ 
+   } else {
+     next();
+   }
+});
+
+export default router
