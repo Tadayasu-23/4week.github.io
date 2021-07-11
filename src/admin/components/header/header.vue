0709988ddcd55a29be5d4b8c0bee2aa375ec11e9
Roman Vorontsov <Tadayasu@yandex.ru>
2021-07-11 10:48:45 +0300

    second commit

---
+++ src/admin/components/header/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header-component">
+    <headline title="Панель администрирования">
+        <user />
+    </headline>
+    <navigation />
+  </div>
+    
+</template>
+
+<script>
+import headline from "../../components/headline/headline.vue";
+import user from "../../components/user/user.vue";
+import navigation from "../../components/navigate/navigation.vue";
+
+export default {
+  components: {
+    headline,
+    user,
+    navigation
+  }
+}
+</script>
---
+++ src/admin/pages/about/about.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="about-page-component">
+    <div class="page-content">
+      <div class="container" v-if="categories.length">
+        <div class="header">
+          <div class="title">Блок "Обо мне"</div>
+          <iconed-button 
+            type="iconed" 
+            v-if="emptyCatIsShown === false"
+            @click="emptyCatIsShown = true" 
+            title="Добавить группу"
+          />
+        </div>
+        <ul class="skills">
+          <li class="item" v-if="emptyCatIsShown">
+            <category 
+              @remove="emptyCatIsShown = false"
+              @approve="createCategory" 
+              empty 
+            />
+          </li>
+          <li 
+            class="item" 
+            v-for="category in categories"
+            :key="category.id"
+          >
+            <category 
+              :title="category.category"
+              :skills="category.skills" 
+              @create-skill="createSkill($event, category.id)"
+              @edit-skill="editSkill"
+              @remove-skill="removeSkill"
+            />
+          </li>
+        </ul>
+      </div>
+      <div class="container" v-else>
+        loading....
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import button from "../../components/button/button.vue";
+import category from "../../components/category/category.vue";
+import { mapActions, mapState } from "vuex";
+
+export default {
+  components: {
+    iconedButton: button,
+    category
+  },
+  data() {
+    return {
+      emptyCatIsShown: false
+    }
+  },
+  computed: {
+    ...mapState('categories', {
+      categories: state => state.data
+    })
+  },
+  methods: {
+    ...mapActions({
+      createCategoryAction: "categories/create",
+      fetchCategoryAction: "categories/fetch",
+      addSkillAction: "skills/add",
+      editSkillAction: "skills/edit",
+      removeSkillAction: "skills/remove"
+    }),
+    async createSkill(skill, categoryId) {
+      const newSkill = {
+        ...skill,
+        category: categoryId
+      }
+      await this.addSkillAction(newSkill);
+      skill.title = '';
+      skill.percent = '';
+    },
+    async editSkill(skill) {
+      await this.editSkillAction(skill);
+      skill.editmode = false;
+    },
+    removeSkill(skill) {
+      this.removeSkillAction(skill);
+    },
+    async createCategory(categoryTitle) {
+      
+      try {
+        await this.createCategoryAction(categoryTitle);
+        this.emptyCatIsShown = false;
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  },
+  created() {
+    this.fetchCategoryAction();
+  }
+};
+</script>
+
+<style lang="postcss" scoped src='./about.pcss'>
+
+</style>
